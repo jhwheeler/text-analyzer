6425ccf81a22d1b41e06eca0d68e4cd4a64dabe1
Alacritas <jhwheeler@gmail.com>
2017-01-09 13:54:39 +0200

    added readme and laid out app logic flow

---
+++ README.md
@@ -0,0 +1,2 @@
+# Text Analyzer
+A basic text analyzing interface showing word count, unique word count, average word length, and average sentence length.
--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// your code here!
+// transform strings into an array of words
+// count the words (as they're items in the array)
+// to check unique word count, compare with for loop and tracking var
+// for average word length, add lengths of words in array and divide by number of words in array
+// for average sentence length...???
