7836a544885fb98db41883045ced0cd29d715514
Alacritas <jhwheeler@gmail.com>
2017-01-09 18:14:29 +0200

    set up word count

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
-// transform strings into an array of words
-// count the words (as they're items in the array)
+$(function() {
+   $('button').click(function() {
+       var text = $('#user-text').val()
+       var words = text.split(" ")
+       var wordCount = $(words).length
+   })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+ * var uniqueWords = []
+ * for (var i = 0; i < words.length; i++) {
+ *  if (i !==
+ *
+
+
+
+
+
+
+
+
+//(done) transform strings into an array of words
+//(done) count the words (as they're items in the array)
 // to check unique word count, compare with for loop and tracking var
 // for average word length, add lengths of words in array and divide by number of words in array
 // for average sentence length...???
+
