bda5353023d789530f37d4ee0afe378af532ab8e
Alacritas <jhwheeler@gmail.com>
2017-01-11 08:24:03 +0200

    worked on unique word count

--- app.js
+++ app.js
@@ -1,30 +1,52 @@
+function getText() {
+    var text = $('#user-text').val()
+    var wordArr = getWordArr(text)
+    return wordArr
+}
+
+function getWordArr(text) {
+    var words = text.split(" ")
+    console.log(words)
+    var uniqueArr = getUniqueArr(words)
+    return uniqueArr
+}
+
+function getUniqueArr(words) {
+    var uniqueWords = {}
+    for (var i = 0; i < words.length; i++) {
+        if (uniqueWords.hasOwnProperty(words.i)) {
+            console.log(uniqueWords)
+        } else {
+           uniqueWords[i] = words[i] 
+    }
+}
+console.log(uniqueWords)
+}
+
+//need to call this somewhere - maybe in getUniqueArr? doesn't seem like the right place, but where?
+function getWordCount(words) {
+    var wordCount = words.length
+    return wordCount
+}
+
+function clickHandler() {
+    $('button').click(function() {
+        getText()
+    })
+}
+
 $(function() {
-   $('button').click(function() {
-       var text = $('#user-text').val()
-       var words = text.split(" ")
-       var wordCount = $(words).length
-   })
+    clickHandler()
 })
 
 
 
-
-
-
-
-
-
-
-
-
-/* 
- * var uniqueWords = []
- * for (var i = 0; i < words.length; i++) {
- *  if (i !==
- *
-
-
-
+//loop through array of words
+//use object to track - object more efficient > array
+//check if property exists
+//assign property for each
+//object.keys()
+//use length
 
 
 
@@ -34,5 +56,5 @@ $(function() {
 //(done) count the words (as they're items in the array)
 // to check unique word count, compare with for loop and tracking var
 // for average word length, add lengths of words in array and divide by number of words in array
-// for average sentence length...???
-
+// for average sentence length: 
+// print each result of analysis in `.text-report` div while removing `.hidden`
