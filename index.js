3ed4844734f3b68320c61fca06e1a327a1542d13
Alacritas <jhwheeler@gmail.com>
2017-01-12 15:33:23 +0200

    finished structure; need to fix bug preventing rendering to DOM

--- index.js
+++ index.js
@@ -1,49 +1,53 @@
 'use strict'
 
-// state variable
+// state object
 
 var state = {
     text: "",
     words: [],
     uniqueWords: [],
-    sentences: []
+    sentences: [],
     wordLengths: [],
-    sentenceLengths: []
+    sentenceLengths: [],
+    wordCount: 0,
+    uniqueWordCount: 0,
+    averageWordLength: 0,
+    averageSentenceLength: 0
 }
 
 
 //state modification functions
 
 var getText = function(state) {
-    state.text = $('user-text').val()
+    state.text = $('#user-text').val()
 }
 
 var getWords = function(state) {
     state.words = state.text.match(/[^_\W]+/g)
+    //need to also change all uppercase to lowercase
 }
 
 var getSentences = function(state) {
     state.sentences = state.text.match( /[^\.!\?]+[\.!\?]+/g )
+    if (state.sentences == null) {
+        state.sentences = state.text
+    }
 }
 
 var getUniqueWords = function(state) {
     for (var i = 0; i < state.words.length; i++) {
-        if (state.uniqueWords.indexOf(words[i]) < 0) {
-            state.uniqueWords.push(words[i])
+        if (state.uniqueWords.indexOf(state.words[i]) < 0) {
+            state.uniqueWords.push(state.words[i])
         }
     }
 }
 
 var getWordCount = function(state) {
-    var wordCount = state.words.length
-    //should I add `wordCount` to `state`?
-    return wordCount
+    state.wordCount = state.words.length
 }
 
 var getUniqueWordCount = function(state) {
-    var uniqueWordCount = state.uniqueWords.length
-    //should I add `uniqueWordCount` to `state`?
-    return uniqueWordCount
+    state.uniqueWordCount = state.uniqueWords.length
 }
 
 var getWordLengths = function(state) {
@@ -56,43 +60,61 @@ var getAverageWordLength = function(state) {
     var sum = state.wordLengths.reduce(function(a, b) {
         return a + b
     }, 0)
-    var averageWordLength = sum/wordLengths.length
-    return averageWordLength
+    state.averageWordLength = (sum/state.wordLengths.length).toFixed(2)
 }
 
 var getSentenceLengths = function(state) {
-    for (var i = 0; i < state.sentences.length; i++) {
-        state.sentenceLengths.push(state.sentences[i].length)
-    }
+        for (var i = 0; i < state.sentences.length; i++) {
+            state.sentenceLengths.push(state.sentences[i].length)
+        }
 }
 
 var getAverageSentenceLength = function(state) {
     var sum = state.sentenceLengths.reduce(function(a,b) {
        return a + b
     }, 0)
-   var averageSentenceLength = sum/sentenceLengths.length
-  return averageSentenceLength
+   state.averageSentenceLength = (sum/state.sentenceLengths.length).toFixed(2)
 }
 
 // render functions
 
-var renderResults = function(state) {
+var renderWordCount = function(state, element) {
     $("dl").toggleClass('hidden')
+    return element.append(state.wordCount)
 }
 
+var renderUniqueWordCount = function(state, element) {
+    $("dl").toggleClass('hidden')
+    return element.append(state.uniqueWordCount)
+}
 
+var renderAverageWordLength = function(state, element) {
+    $("dl").toggleClass('hidden')
+    return element.append(state.averageWordLength)
+}
+
+var renderAverageSentenceLength = function(state, element) {
+    $("dl").toggleClass('hidden')
+    return element.append(state.averageSentenceLength)
+}
 
 // event listener functions
 
 $(function() {
     $('button').click(function() {
-        event.preventDefault()
         getText(state)
         getWords(state)
         getSentences(state)
         getUniqueWords(state)
         getWordCount(state)
         getUniqueWordCount(state)
-
-    }
-}
+        getWordLengths(state)
+        getAverageWordLength(state)
+        getSentenceLengths(state)
+        getAverageSentenceLength(state)
+        renderWordCount(state, $('.wordCount'))
+        renderUniqueWordCount(state, $('.uniqueWordCount'))
+        renderAverageWordLength(state, $('.averageWordLength'))
+        renderAverageSentenceLength(state, $('.averageSentenceLength'))
+    })
+})
